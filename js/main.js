a5d564e2e4e6ed7f7c6abff3e258424fb4064468
AgustinaDiTommaso <89307742+AgustinaDiTommaso@users.noreply.github.com>
2023-06-29 11:49:06 -0300

    Add image user y fix control de display

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let users = [
     {
       "nombre": "Juan",
       "apellido": "Pérez",
+      "usuario" : "JuanPerez",
       "password" : "1234",
       "materia": "Historia",
       "tipo": "user"
@@ -34,28 +35,38 @@ let nav_admin=document.getElementById("nav-admin");
 let nav_inicioSesion=document.getElementById("nav-inicioSesion");
 
 
-//CONTROL DE SESION
+//CONTROL DE VISTAS
 
 let btnIniciarSesion= document.getElementById("btn-sesion");
 btnIniciarSesion.addEventListener('click', function(e){
     hiddenAll();    
     nav_inicioSesion.style.display="initial";
-    btnIniciarSesion.style.visibility="hidden";
+    
     
     loginUsuario();
 
 })
 
 
+
 // FUNCION DE LOOP QUE CONTROLA SESION DE USUARIO
 function controllerUsers(){
+  
   if(userActual==users_type[2]){
+    console.log("El usuario logueado es un admin");
     hiddenAll();
     nav_admin.style.display="initial";
+    btnEndSesion.style.display="initial";
   }
   else if(userActual==users_type[1]){
+    console.log("El usuario logueado es un usuario común");
     hiddenAll();
     nav_user.style.display="initial";
+    btnEndSesion.style.display="initial";
+  }else{
+    hiddenAll();
+    nav_public.style.display="initial";
+    btnIniciarSesion.style.display="initial";
   }
   
   
@@ -63,26 +74,37 @@ function controllerUsers(){
 //OBTIENE DATOS DEL FORM  PARA INICIAR SESION
 function obtenerDatosForm(){
   let form= document.getElementById("formsesion");
-  inputUser=form.elements["usuario"];
-  inputPassword= form.elements["password"];
+  inputUser=form.elements["usuario"].value;
+  inputPassword= form.elements["password"].value;
+  console.log(inputUser +" : "+inputPassword);
+  loginUsuario();
 }
 
 //VERIFICA QUE EXISTA UN USUARIO CON ESE NOMBRE Y CONTRASEÑA, Y GUARDA EL TIPO DE USUARIO. (NO MUESTRA MSJ ERROR POR AHORA)
 function loginUsuario(){
-  if(inputUser!==null && inputPassword!==null){
-    let usuarioEncontrado= users.find(function(user){
-      if(user.usuario===inputUser && user.password=== inputPassword){
-        return user.tipo;
+  let usuarioEncontrado= null;
+  //Si los datos del form no estan vacíos
+  if(inputUser!=null && inputPassword!=null){
+    console.log("estoy buscando el usuario en el arreglo json")
+    //Busca si existe un usuario con ese nombre y contraseña, si lo encuentra retorna el tipo sino null
+    usuarioEncontrado= users.find(function(user){
+      if(user.usuario==inputUser && user.password== inputPassword){
+        return user;
       }else{
         return null;
       }
     });
-    if(usuarioEncontrado!==null){
-      switch(usuarioEncontrado){
+    //Si encontró el usuario
+    if(usuarioEncontrado!=null){
+      console.log("encontre el usuario en el arreglo json"+ usuarioEncontrado.tipo)
+      //Almacena el tipo de usuario en la variable global userActual
+      switch(usuarioEncontrado.tipo){
         case "user":
+          
           userActual=users_type[1];
           break;
         case "admin":
+          console.log(userActual)
           userActual= users_type[2];
           break;
       }
@@ -96,6 +118,9 @@ function hiddenAll(){
   nav_user.style.display="none" ;
   nav_admin.style.display="none";
   nav_inicioSesion.style.display="none";
+
+  btnEndSesion.style.display="none";
+  btnIniciarSesion.style.display="none";
 }
 
 
