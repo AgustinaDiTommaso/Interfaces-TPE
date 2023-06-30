1e27db98bb672fbc4fcae9d21ce28090cabbd9e9
AgustinaDiTommaso <89307742+AgustinaDiTommaso@users.noreply.github.com>
2023-06-30 19:51:05 -0300

    Fix CerrarSesion

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 // DATOS DE USUARIO
-let users_type=["public", "user", "admin"];
+let users_type = ["public", "user", "admin"];
 let users = [
-    {
-      "nombre": "Juan",
-      "apellido": "Pérez",
-      "usuario" : "JuanPerez",
-      "password" : "1234",
-      "materia": "Historia",
-      "tipo": "user"
-    },
-    
-    {
-        "nombre": "Administrador",
-        "apellido": "",
-        "usuario" : "administrador",
-        "password" :"1234",
-        "materia": "",
-        "tipo": "admin"
-      }
-  ];
-let userActual= users_type[0];
+  {
+    "nombre": "Juan",
+    "apellido": "Pérez",
+    "usuario": "JuanPerez",
+    "password": "1234",
+    "materia": "Historia",
+    "tipo": "user"
+  },
+
+  {
+    "nombre": "Administrador",
+    "apellido": "",
+    "usuario": "administrador",
+    "password": "1234",
+    "materia": "",
+    "tipo": "admin"
+  }
+];
+let userActual = users_type[0];
 let inputUser;
 let inputPassword;
 
@@ -27,119 +27,123 @@ let inputPassword;
 
 
 // DISPLAY 
-let nav= document.getElementById("nav");
-let nav_public=document.getElementById("nav-public");
-let nav_user=document.getElementById("nav-user");
-let nav_admin=document.getElementById("nav-admin");
-let nav_inicioSesion=document.getElementById("nav-inicioSesion");
+let nav = document.getElementById("nav");
+let nav_public = document.getElementById("nav-public");
+let nav_user = document.getElementById("nav-user");
+let nav_admin = document.getElementById("nav-admin");
+let nav_inicioSesion = document.getElementById("nav-inicioSesion");
 
-let container_main= document.getElementById("container-main");
-let container_secondary= document.getElementById("container-secondary");
+let container_main = document.getElementById("container-main");
+let container_secondary = document.getElementById("container-secondary");
 
-let noticia= document.getElementById("noticia")
-let noticias_view= document.getElementById("noticias-View");
+let noticia = document.getElementById("noticia")
+let noticias_view = document.getElementById("noticias-View");
 
 //BOTONES
-let btnIniciarSesion= document.getElementById("btn-sesion");
-let btnEndSesion= document.getElementById("btn-cerrarSesion");
-let btnMenu= document.getElementById("menu");
-let btn_verMas= document.querySelectorAll("btn-verMas");
+let btnIniciarSesion = document.getElementById("btn-sesion");
+let btnEndSesion = document.getElementById("btn-cerrarSesion");
+let btnMenu = document.getElementById("menu");
+let btn_verMas = document.querySelectorAll("btn-verMas");
 
 
 //CONTROL DE VISTAS
 
 //- Eventos
 
-btnIniciarSesion.addEventListener('click', function(e){
-    hiddenAll();    
-    nav_inicioSesion.classList.remove("desaparecer");  
-    container_secondary.classList.add("backgroundEscuelaImg");
-    loginUsuario();
+btnIniciarSesion.addEventListener('click', function (e) {
+  hiddenAll();
+  nav_inicioSesion.classList.remove("desaparecer");
+  container_secondary.classList.add("backgroundEscuelaImg");
+
 
 })
+btnEndSesion.addEventListener('click', function (e) {
+  userActual= users_type[0];
+  hiddenAll();
+  nav_inicioSesion.classList.remove("desaparecer");
+  container_secondary.classList.add("backgroundEscuelaImg");
+  
 
-btnMenu.addEventListener('click', function(e){
+}
+)
+
+btnMenu.addEventListener('click', function (e) {
   container_main.classList.toggle("desaparecer");
 
-  btnMenu.classList.toggle("menu-oculto")  ;
+  btnMenu.classList.toggle("menu-oculto");
   btnMenu.classList.toggle("menu-desplegado");
   container_secondary.classList.toggle("widht70");
   container_secondary.classList.toggle("widht100");
 })
-for(let i=0; i<btn_verMas.length;i++){
-  btn_verMas[i].addEventListener("click",function(e){
-    mostrarNoticia(i);
-  })
-}
 
-function main(){
+function main() {
   hiddenAll();
   controllerUsers();
 }
 // FUNCION QUE CONTROLA SESION DE USUARIO y LA VISTA
-function controllerUsers(){
-  
-  if(userActual==users_type[2]){
+function controllerUsers() {
+
+  if (userActual == users_type[2]) {
     console.log("El usuario logueado es un admin");
     hiddenAll();
     nav_admin.classList.remove("desaparecer");
     noticias_view.classList.remove("desaparecer");
     btnEndSesion.classList.remove("desaparecer");
   }
-  else if(userActual==users_type[1]){
+  else if (userActual == users_type[1]) {
     console.log("El usuario logueado es un usuario común");
     hiddenAll();
     nav_user.classList.remove("desaparecer");
     noticias_view.classList.remove("desaparecer");
     btnEndSesion.classList.remove("desaparecer");
-  }else{
+  } else {
     hiddenAll();
     nav_public.classList.remove("desaparecer");
     nav_public.classList.remove("desaparecer");
     noticias_view.classList.remove("desaparecer");
     btnIniciarSesion.classList.remove("desaparecer");
   }
-  
-  
+
+
 }
 //OBTIENE DATOS DEL FORM  PARA INICIAR SESION
-function obtenerDatosForm(){
-  let form= document.getElementById("formsesion");
-  inputUser=form.elements["usuario"].value;
-  inputPassword= form.elements["password"].value;
-  console.log(inputUser +" : "+inputPassword);
+function obtenerDatosForm() {
+  let form = document.getElementById("formsesion");
+  inputUser = form.elements["usuario"].value;
+  inputPassword = form.elements["password"].value;
+  console.log(inputUser + " : " + inputPassword);
   loginUsuario();
 }
 
 //VERIFICA QUE EXISTA UN USUARIO CON ESE NOMBRE Y CONTRASEÑA, Y GUARDA EL TIPO DE USUARIO. (NO MUESTRA MSJ ERROR POR AHORA)
-function loginUsuario(){
-  let usuarioEncontrado= null;
+function loginUsuario() {
+  let usuarioEncontrado = null;
   //Si los datos del form no estan vacíos
-  if(inputUser!=null && inputPassword!=null){
+  if (inputUser != null && inputPassword != null) {
     console.log("estoy buscando el usuario en el arreglo json")
     //Busca si existe un usuario con ese nombre y contraseña, si lo encuentra retorna el tipo sino null
-    usuarioEncontrado= users.find(function(user){
-      if(user.usuario==inputUser && user.password== inputPassword){
+    usuarioEncontrado = users.find(function (user) {
+      if (user.usuario == inputUser && user.password == inputPassword) {
         return user;
-      }else{
+      } else {
         return null;
       }
     });
     //Si encontró el usuario
-    if(usuarioEncontrado!=null){
-      
-      
-      console.log("encontre el usuario en el arreglo json"+ usuarioEncontrado.tipo)
+    if (usuarioEncontrado != null) {
+
+
+      console.log("encontre el usuario en el arreglo json" + usuarioEncontrado.tipo)
       //Almacena el tipo de usuario en la variable global userActual
-      switch(usuarioEncontrado.tipo){
+      switch (usuarioEncontrado.tipo) {
         case "user":
-          
-          userActual=users_type[1];
-          
+
+          userActual = users_type[1];
+
           break;
         case "admin":
           console.log(userActual)
-          userActual= users_type[2];
+          userActual = users_type[2];
           break;
       }
       container_secondary.classList.remove("backgroundEscuelaImg");
@@ -147,24 +151,18 @@ function loginUsuario(){
     }
   }
 }
-/*
-function mostrarNoticia(i){
-  let noticias= document.getElementById("contenedor-noticias");
-  noticias.style.display="none";
-  
 
-}*/
 
 //OCULTA TODOS LOS DISPLAY
-function hiddenAll(){
-  nav_public.classList.add("desaparecer") ;
-  nav_user.classList.add("desaparecer") ;
-  nav_admin.classList.add("desaparecer") ;
-  nav_inicioSesion.classList.add("desaparecer") ;
-  noticias_view.classList.add("desaparecer") ;
+function hiddenAll() {
+  nav_public.classList.add("desaparecer");
+  nav_user.classList.add("desaparecer");
+  nav_admin.classList.add("desaparecer");
+  nav_inicioSesion.classList.add("desaparecer");
+  noticias_view.classList.add("desaparecer");
 
-  btnEndSesion.classList.add("desaparecer") ;
-  btnIniciarSesion.classList.add("desaparecer") ;
+  btnEndSesion.classList.add("desaparecer");
+  btnIniciarSesion.classList.add("desaparecer");
 }
 
 
